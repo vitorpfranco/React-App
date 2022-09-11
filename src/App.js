865dbaf679de4c4a078de9b9e0a4f2a56a6629ff
vitorpfranco <vitor.pereira.franco@gmail.com>
2022-09-11 15:51:07 -0300

    Add main pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import About from './Components/pages/About';
+import Contact from './Components/pages/Contact';
+import Home from './Components/pages/Home';
+import Projects from './Components/pages/Projects';
 function App() {
   return (
-    <div className="App">
-<p>costs</p>
-    </div>
+<Router>
+  <ul>
+    <li>Home</li>
+    <li>Contato</li>
+  </ul>
+  <Routes>
+    <Route exact path="/" element={<Home/>}/>
+    <Route path="/sobre" element={<About/>}/>
+    <Route path="/contato" element={<Contact/>}/>
+    <Route path="/projetos" element={<Projects/>}/>
+  </Routes>
+</Router>
   );
 }
 
